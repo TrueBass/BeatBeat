fb1324e54ca08a7a1fc74f393f587c6e38eea574
IQlikeinBREAD <118021713+IQlikeinBREAD@users.noreply.github.com>
2024-11-21 21:33:53 +0100

    Dodanie ProfileScreen

---
+++ src/ProfileScreen.js
@@ -0,0 +1,13 @@
+import { React, useState } from 'react';
+import { View,Image, StyleSheet} from 'react-native';
+
+import InputField from './components/InputField';
+import BeatButton from './components/BeatButton';
+
+export default function ProfileScreen(){
+    return(
+        <View>
+            
+        </View>
+    )
+}
